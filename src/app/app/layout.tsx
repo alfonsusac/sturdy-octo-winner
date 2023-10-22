04e71f3fa374b5bb1960b1139d9bb266ac4a72dd
Alfonsus Ardani <alfonsusac@gmail.com>
2023-10-22 20:54:27 +0700

    try to add ui

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,13 +1,15 @@
-import Providers from "./layout.client"
 import { getSession } from "@/lib/next-auth"
+import { BaseLayout, Providers } from "./layout.client"
 
 export default async function AppLayout(p: {
   children: React.ReactNode
 }) {
   const session = await getSession()
   return (
-    <Providers session={session} >
-      {p.children}
+    <Providers session={ session }>
+      <BaseLayout>
+        {p.children}
+      </BaseLayout>
     </Providers>
   )
 }
