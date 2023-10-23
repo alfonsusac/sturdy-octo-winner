22d396fd3039c58477ede2325223d6fc84162cc9
Alfonsus Ardani <alfonsusac@gmail.com>
2023-10-23 16:29:53 +0700

    added bg to auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -5,10 +5,10 @@ import { Icon } from '@iconify/react'
 export default function AuthPage() {
   return (
     <main className="min-h-screen w-full gap-8 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold text-white">
-        Log in
-      </h1>
-      <div className="max-w-xs w-full">
+      <div className="max-w-sm w-full flex flex-col gap-8 items-center bg-zinc-900/50 p-8 rounded-3xl">
+        <h1 className="text-3xl font-bold text-white">
+          Log in
+        </h1>
         <LoginGoogleButton secondary large block />
       </div>
     </main>
