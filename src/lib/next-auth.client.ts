5376cdb1949fd91584b3e1625d9d0d989924c1d0
Alfonsus Ardani <alfonsusac@gmail.com>
2023-10-31 13:58:55 +0700

    add uploadthing

--- src/lib/next-auth.client.ts
+++
@@ -1,7 +0,0 @@
-import { useSession as useNextAuthSession } from "next-auth/react"
-
-export function useSession() {
-  const session = useNextAuthSession()
-  if (!session) alert("Session Not Found, it should be found as it is prefetched from the server.")
-  return session
-}
