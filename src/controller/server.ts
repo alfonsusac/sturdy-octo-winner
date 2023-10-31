b5a667e696ab67e0327bafdcb370d69eb0d97179
Alfonsus Ardani <alfonsusac@gmail.com>
2023-10-31 20:07:21 +0700

    adding server and user controller

---
+++ src/controller/server.ts
@@ -0,0 +1,29 @@
+import { getSession } from "@/lib/auth/next-auth"
+import prisma from "@/lib/db/prisma"
+import "server-only"
+
+/**
+ * Create a server and make the logged in user as owner
+ */
+export async function createServer({ name, profilePicture }: {
+  name: string,
+  profilePicture: string,
+  ownerID: string
+}) {
+
+  const { email } = await getSession()
+  const newServer = await prisma.server.create({
+    data: {
+      name,
+      profilePicture,
+      owner: { connect: { email } }
+    }
+  })
+
+  return newServer
+
+}
+
+
+
+
--- src/controller/user.ts
+++ src/controller/user.ts
@@ -3,50 +3,40 @@ import prisma from "@/lib/db/prisma"
 import { redirect } from "next/navigation"
 import { memoize } from "nextjs-better-unstable-cache"
 import { cache } from "react"
+import "server-only"
 
-class LoggedInUser {
-  static getData = cache(async () => {
-    const { email } = await getSession()
-    const user = await prisma.user.findUnique({
-      where: { email }
-    })
-    if (!user) redirect('/register')
-    // Redirect to /register where user can get onboarded
-    //  to fill necessary data on the database'
-    return user
+/**
+ * Get Logged In User's Data.
+ */
+export const getUserData = cache(async () => {
+
+  const { email } = await getSession()
+  const user = await prisma.user.findUnique({
+    where: { email }
   })
 
-  // Close to idempotent so cache is necessary
-  static getDefaultImage = memoize(async (email: string) => {
+  if (!user) redirect('/register')
+  else return user
+  
+})
+
+
+/**
+ * Get OAuth User's Default Image.
+ * If doesn't exist, generate it
+ */
+export const getUserDefaultImage = memoize(
+
+  async (email: string) => {
     const userDefaultImage = await prisma.userDefaultImage.findUnique({ where: { email } })
     if (!userDefaultImage)
       await prisma.userDefaultImage.create({ data: { email } })
-    return userDefaultImage
-  }, {
+    else return userDefaultImage
+  },
+
+  {
     log: ['datacache'],
     logid: "Get User Default Image"
-  })
-}
-
-
-export const getUserData = cache(async () => {
-    const { email } = await getSession()
-    const user = await prisma.user.findUnique({
-      where: { email }
-    })
-    if (!user) redirect('/register')
-    // Redirect to /register where user can get onboarded
-    //  to fill necessary data on the database'
-    return user
-})
-
-export const getUserDefaultImage = memoize(async (email: string) => {
-  const userDefaultImage = await prisma.userDefaultImage.findUnique({ where: { email } })
-  if (!userDefaultImage)
-    await prisma.userDefaultImage.create({ data: { email } })
-  return userDefaultImage
-}, {
-  log: ['datacache'],
-  logid: "Get User Default Image"
-})
+  }
+)
 
