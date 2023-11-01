604cb794b265e19d5934cb5aa12d1d4e66bf1909
Alfonsus Ardani <alfonsusac@gmail.com>
2023-11-02 00:18:47 +0700

    added logger

--- src/lib/devutil.ts
+++ src/lib/devutil.ts
@@ -1,3 +1,22 @@
+import chalk from "chalk"
+import path from "path"
+
 export function delay(ms: number) {
   return new Promise(res => setTimeout(res, ms))
+}
+
+export function getExt(filename: string) {
+  const extdot = path.extname(filename)
+  if (extdot.split('.')[1]) {
+    return extdot.split('.')[1]
+  } else {
+    throw new Error("This file has no extension")
+  }
+}
+
+export function logFunc(obj: any) {
+  console.log(chalk.white(obj))
+}
+export function logAuth(obj: any) {
+  console.log(chalk.yellow(obj))
 }
