13aa59dd6317bbd93f05c42738af0839e237aad4
Alfonsus Ardani <alfonsusac@gmail.com>
2023-11-02 00:19:55 +0700

    added S3 Client

--- src/lib/upload/config.ts
+++ src/lib/upload/config.ts
@@ -1,5 +1,6 @@
 import { NextUploadConfig } from "next-upload"
 import { env } from "../env"
+import { S3Client } from "@aws-sdk/client-s3"
 
 export const config: NextUploadConfig = {
   maxSize: '2mb',
@@ -10,3 +11,13 @@ export const config: NextUploadConfig = {
     secretKey: env("R2_SECRET_ACCESS_KEY"),
   }
 }
+
+
+export const S3 = new S3Client({
+  region: "auto",
+  endpoint: env("R2_ENDPOINTS_S3_CLIENT"),
+  credentials: {
+    accessKeyId: env("R2_ACCESS_KEY_ID"),
+    secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+  },
+})
