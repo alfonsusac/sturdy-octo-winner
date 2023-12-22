53144b9c724faefba07fe8499c14ec36e8525b19
Alfonsus Ardani <alfonsusac@gmail.com>
2023-12-22 17:34:46 +0700

    modal refactor

--- src/components/base/modal.tsx
+++ src/components/base/modal.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from '@/lib/tailwind'
 import * as Dialog from '@radix-ui/react-dialog'
-import { useEffect, useState } from "react"
+import { ReactNode, useEffect, useState } from "react"
 
 /**
   Modal Base Component
@@ -11,6 +11,36 @@ import { useEffect, useState } from "react"
   
  
  */
+
+function useCloseAnimation(duration = 200, events?: {
+  onOpen?: () => void,
+  onClose?: () => void,
+  duration?: number
+}) {
+  const [isVisible, setVisible] = useState(false)
+  const [isOpen, setOpen] = useState(false)
+
+  // Callbacks
+  const handleOpenChange = (open: boolean) => {
+    if (open) {
+      setVisible(true)
+      events?.onOpen?.()
+    }
+    if (!open) {
+      setOpen(false)
+      events?.onClose?.()
+      setTimeout(() => setVisible(false), duration)
+    }
+  }
+
+  useEffect(() => {
+    if (isVisible) {
+      setOpen(true)
+    }
+  }, [isVisible])
+
+  return { isVisible, isOpen, handleOpenChange }
+}
 
 export function ModalBase(p: {
   trigger: React.ReactNode
@@ -22,84 +52,47 @@ export function ModalBase(p: {
   onClose?: () => void,
   onOpen?: () => void,
 }) {
-  // States
-  const [visible, setVisible] = useState(false)
-  const [open, setOpen] = useState(false)
-  // Callbacks
-  
-  const handleOpenChange = (open: boolean) => {
-    if (open) {
-      setVisible(true)
-      p.onOpen?.()
-    }
-    if (!open) {
-      setOpen(false)
-      p.onClose?.()
-      setTimeout(() => setVisible(false), 200)
-    }
-  }
-
-  // Effects
-
-
-  useEffect(() => {
-    if (visible) {
-      setOpen(true)
-    }
-  }, [visible])
-  
-  return <Dialog.Root open={ visible } onOpenChange={ handleOpenChange }>
+  const { isVisible, isOpen, handleOpenChange } = useCloseAnimation(200, p)
+  return <Dialog.Root open={ isVisible } onOpenChange={ handleOpenChange }>
     <Dialog.Trigger asChild>{ p.trigger }</Dialog.Trigger>
     <Dialog.Portal>
-      <DialogOverlay isOpen={open} className={p.className?.overlay} />
-      <Dialog.Content data-state-transition={ open } className={ cn(
-          "fixed",
-
-          "focus:outline-none",
-          "bg-[#272B3C]",
-          "flex flex-row justify-center items-stretch",
-
-          "w-full",
-          "max-w-sm",
-          "text-sm",
-          "rounded-2xl",
-          "overflow-clip",
-
-          // Center
-          "top-1/2 -translate-y-1/2",
-          "left-1/2 -translate-x-1/2",
-
-          //Transition
-          "transition-all duration-300",
-          // initial state
-          "opacity-0",
-          "scale-0",
-          // open state
-          "data-[state-transition=true]:opacity-100",
-          "data-[state-transition=true]:scale-100",
-
-          // "focus:outline-indigo-600",
-          "outline-indigo-600",
-
-          p.className?.content
-        ) }
-      >
-        { visible && p.children }
-      </Dialog.Content>
+      <Overlay isOpen={ isOpen } className={ p.className?.overlay } />
+      <Content isOpen={ isOpen } className={ p.className?.content }>
+        { isVisible && p.children }
+      </Content>
     </Dialog.Portal>
   </Dialog.Root>
 
 }
 
-function DialogOverlay(p: { className?: string, isOpen?: boolean }) {
-  return <Dialog.Overlay data-state-transition={ p.isOpen }  className={ cn(
-      "fixed inset-0",
-      // "bg-red-500",
-      "bg-black",
-      "transition-all duration-300",
-      "opacity-0",
-      "data-[state-transition=true]:opacity-70",
-      p?.className,
-    ) }
-  />
+function Overlay(p: { className?: string, isOpen?: boolean }) {
+  return <Dialog.Overlay data-state-transition={ p.isOpen } className={ cn(
+    "fixed inset-0 bg-black",
+    "transition-all duration-300",
+    "opacity-0",
+    "data-[state-transition=true]:opacity-70",
+    p?.className,
+  ) } />
+}
+
+function Content(p: { className?: string, isOpen?: boolean, children: ReactNode }) {
+  return <Dialog.Content data-state-transition={ p.isOpen } className={ cn(
+    "fixed outline-none",
+    "top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2", //Center
+    "w-full max-w-sm", // Default Size of Dialog
+    "bg-[#272B3C]", // Default BG color
+    "flex flex-row justify-center items-stretch", // Default Layout
+    "text-sm",
+    "rounded-2xl",
+    "overflow-clip",
+    // Transition
+    "transition-all duration-300",
+    //  initial state
+    "opacity-0",
+    "scale-0",
+    //  open state
+    "data-[state-transition=true]:opacity-100",
+    "data-[state-transition=true]:scale-100",
+    p.className
+  ) }>{ p.children }</Dialog.Content>
 }
