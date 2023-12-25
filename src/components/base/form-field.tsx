8ac52ebbd6ccda77683deffa2bf56564fb65fe57
Alfonsus Ardani <alfonsusac@gmail.com>
2023-12-25 19:53:30 +0700

    label

--- src/components/base/form-field.tsx
+++ src/components/base/form-field.tsx
@@ -1,19 +1,12 @@
 import { cn } from "@/lib/tailwind"
-import { ComponentProps, Ref, createContext, forwardRef } from "react"
+import { ComponentProps, ElementRef, Ref, createContext, forwardRef } from "react"
 
-
-
-
-
-
-export const FormMessage = forwardRef(
-  function FormMessage(p: ComponentProps<"p">, ref: Ref<HTMLParagraphElement>) {
-    const formMessageId = ""
-
-    return (
-      <p ref={ ref } id={ formMessageId } className={ cn("", p.className) }
-      >
-      </p>
-    )
-  }
-)
+export const FormLabel = forwardRef(function FormLabel(
+  prop,
+  ref: HTMLLabelElement
+) {
+  
+  return (
+    <label ref={ refs } />
+  )
+})
