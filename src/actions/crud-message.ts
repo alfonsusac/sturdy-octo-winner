b615986b9e5b01b05200651d7ad974852d63a940
Alfonsus Ardani <alfonsusac@gmail.com>
2024-02-05 18:12:35 +0700

    chore: rename

--- src/actions/crud-message.ts
+++ src/actions/crud-message.ts
@@ -6,10 +6,15 @@ import prisma from "@/lib/db/prisma"
 export async function s_sendMessage(
   params: {
     userid: string
-    channelid: string
+    channelid?: string
+    directMessageRoomId?: string
     message: string
   }
 ) {
+  if (params.channelid && params.directMessageRoomId) {
+    throw new Error("Can't send to channel and direct message at the same time. Please only provide one")
+  }
+
   const user = await Auth.getUserSession()
   if (user.id !== params.userid)
     return { error: "Not Authenticated" }
@@ -24,7 +29,8 @@ export async function s_sendMessage(
       data: {
         content: params.message,
         channelId: params.channelid,
-        sender: params.userid
+        directMessageRoomId: params.directMessageRoomId,
+        sender: params.userid,
       }
     })
     return { data: message }
