495b9744321a7a4451e632224bbbeb2cc72dd366
Alfonsus Ardani <alfonsusac@gmail.com>
2024-02-06 12:37:04 +0700

    chore: refactor

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,24 +1,34 @@
 import { PrismaClient } from '@prisma/client'
+import { createSingleton } from '../singleton'
 
-const prismaClientSingleton = () => {
+export default createSingleton('prisma', () => {
   console.log("Creating New Prisma Client...")
   return new PrismaClient({
     errorFormat: 'pretty',
-    // log: ['info', 'warn', 'error', 'query']
+    log: ['info', 'warn', 'error'],
   })
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
-}
+})
 
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+// const prismaClientSingleton = () => {
+//   console.log("Creating New Prisma Client...")
+//   console.log("Prisma in globalThis?", 'prisma' in globalThis)
+//   return new PrismaClient({
+//     errorFormat: 'pretty',
+//     // log: ['info', 'warn', 'error', 'query']
+//     log: ['info', 'warn', 'error'],
+//   })
+// }
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
 
-export default prisma
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: ReturnType<typeof prismaClientSingleton>
+// }
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma
+// }
