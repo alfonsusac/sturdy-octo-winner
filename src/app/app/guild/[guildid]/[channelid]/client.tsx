7c5eca9ea11b1ef6d3a259eb6ef3ec7b3bb769c5
Alfonsus Ardani <alfonsusac@gmail.com>
2024-02-12 22:00:49 +0700

    query + scroll

--- src/app/app/guild/[guildid]/[channelid]/client.tsx
+++ src/app/app/guild/[guildid]/[channelid]/client.tsx
@@ -10,56 +10,45 @@ import { UndefinedInitialDataOptions, useQuery, useQueryClient } from "@tanstack
 import { FormEvent, useEffect, useRef, useState } from "react"
 import ReactTextareaAutosize from "react-textarea-autosize"
 import { toast } from "sonner"
+import { useChannelMessages } from "./query"
 
 
 
-export let addMessageToList: ((message: Message) => void)
-export let removeMessageFromList: ((id: string) => void)
-export function useMessages(
-  channelid: string,
-  options?: Omit<UndefinedInitialDataOptions<Message[]>, 'queryKey'>
-) {
-  return useQuery<Message[]>({
-    queryKey: ['channel', channelid, 'messages'],
-    ...options
-  })
-}
 
 export function MessageList(
   props: {
     channelid: string
   }
 ) {
-  const queryClient = useQueryClient()
-  const { data: messages } = useMessages(props.channelid)
+  const { data: messages } = useChannelMessages([props.channelid])
   const listRef = useRef<HTMLDivElement>(null)
 
-  const [newMessageEntered, setNewMessageEntered] = useState(false)
+  // const [newMessageEntered, setNewMessageEntered] = useState(false)
 
-  addMessageToList = (message) => {
-    queryClient.setQueryData(['channel', props.channelid, 'messages'], (prev: Message[]) => [message, ...prev])
-    setNewMessageEntered(true)
-  }
+  // addMessageToList = (message) => {
+  //   addMessage(message)
+  //   setNewMessageEntered(true)
+  // }
+
   useEffect(() => {
-    if (newMessageEntered) {
-      listRef.current?.scrollTo({ top: listRef.current.scrollHeight })
-      setNewMessageEntered(false)
+    // if (newMessageEntered) {
+    // listRef.current?.scrollTo({ top: listRef.current.scrollHeight })
+    if (listRef.current) {
+      listRef.current.scrollTop = listRef.current.scrollHeight
     }
-  }, [newMessageEntered])
-
-  removeMessageFromList = (id) => {
-    queryClient.setQueryData(['channel', props.channelid, 'messages'], (prev: Message[]) => prev.filter(g => g.id !== id))
-  }
+    // setNewMessageEntered(false)
+    // }
+  }, [messages])
 
   return (
     <div className="bg-blue-500/0 h-full flex">
       <div
-        ref={ listRef }
+        ref={listRef}
         className="grow max-h-full w-full flex flex-col-reverse mt-auto bg-indigo-400/0 py-2 overflow-y-scroll gap-1 pb-4"
       >
         {
           messages?.map((message, i) => (
-            <MessageItem key={ i } message={ message } />
+            <MessageItem key={i} message={message} />
           ))
         }
       </div>
@@ -93,19 +82,19 @@ function MessageItem(
       flex gap-3
     ">
       <div className="rounded-full overflow-hidden w-8 h-8 shrink-0">
-        <img src={ senderUser?.image } />
+        <img src={senderUser?.image} />
       </div>
       <div className="">
         <div className="flex gap-2 items-baseline">
           <div className="">
-            { senderUser?.name }
+            {senderUser?.name}
           </div>
           <div className="text-indigo-300/30 text-[0.65rem] font-medium">
-            { message.created_at.toUTCString() }
+            {message.created_at.toUTCString()}
           </div>
         </div>
         <div className="font-normal opacity-80">
-          { message.content.split('\n').map((l, i) => <p key={ i }>{ l }</p>) }
+          {message.content.split('\n').map((l, i) => <p key={i}>{l}</p>)}
         </div>
       </div>
     </div>
@@ -120,6 +109,7 @@ export function ChatInput(
 ) {
   const ref = useRef<HTMLTextAreaElement>(null)
   const session = useSession()
+  const { addMessage } = useChannelMessages([props.channelid])
   const [value, setValue] = useState("")
 
   async function submitHandler(event: FormEvent<HTMLFormElement>) {
@@ -136,7 +126,7 @@ export function ChatInput(
         channelid: props.channelid,
         message: value
       })
-      addMessageToList(message)
+      addMessage(message)
 
     } catch (error) {
       console.log(error)
@@ -147,39 +137,41 @@ export function ChatInput(
   const formref = useRef<HTMLFormElement>(null)
   const buttonref = useRef<HTMLButtonElement>(null)
 
-  return <form
-    onSubmit={ submitHandler }
-    className="w-full m-0 p-0"
-    autoComplete="off"
-    name="chat-input"
-    ref={ formref }
-  >
-    <ReactTextareaAutosize
-      value={ value }
-      onChange={ e => {
-        e.preventDefault()
-        if (e.currentTarget.value.at(-1) === '\n') {
+  return (
+    <form
+      onSubmit={submitHandler}
+      className="w-full m-0 p-0"
+      autoComplete="off"
+      name="chat-input"
+      ref={formref}
+    >
+      <ReactTextareaAutosize
+        value={value}
+        onChange={e => {
+          e.preventDefault()
+          if (e.currentTarget.value.at(-1) === '\n') {
 
-        } else {
-          setValue(e.currentTarget.value)
-        }
-      } }
-      name="message"
-      className="bg-transparent placeholder:text-indigo-300/30 focus:outline-none max-w-none text-sm text-wrap whitespace-normal block w-full h-9 p-2 resize-none"
-      placeholder="Message #channel"
-      autoComplete="nope"
-      autoCorrect="nope"
-      ref={ ref }
-      onKeyDown={ (event) => {
-        if (event.key === "Enter" && !event.shiftKey) {
-          formref.current?.requestSubmit(buttonref.current)
-        }
-        if (event.key === "Enter" && event.shiftKey) {
-          setValue(prev => prev + "\n")
-        }
-      } }
-    />
-    <button type="submit" className="!hidden" ref={ buttonref } />
-  </form>
+          } else {
+            setValue(e.currentTarget.value)
+          }
+        }}
+        name="message"
+        className="bg-transparent placeholder:text-indigo-300/30 focus:outline-none max-w-none text-sm text-wrap whitespace-normal block w-full h-9 p-2 resize-none"
+        placeholder="Message #channel"
+        autoComplete="nope"
+        autoCorrect="nope"
+        ref={ref}
+        onKeyDown={(event) => {
+          if (event.key === "Enter" && !event.shiftKey) {
+            formref.current?.requestSubmit(buttonref.current)
+          }
+          if (event.key === "Enter" && event.shiftKey) {
+            setValue(prev => prev + "\n")
+          }
+        }}
+      />
+      <button type="submit" className="!hidden" ref={buttonref} />
+    </form>
+  )
 
 }
